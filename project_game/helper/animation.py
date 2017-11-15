a8b302279f5f7cd307f5f3daa2ad055b71c971d7
Darius Wattimena <dariuswattimena@hotmail.com>
2017-11-15 12:36:01 +0100

    Added animation class

--- project_game/helper/animation.py
+++ project_game/helper/animation.py
@@ -1,4 +1,22 @@
 from image import Image
+import pygame
 
-class Animation:
+class Animation(pygame.sprite.Sprite):
+    def __init__(self):
+        self.images = []
+        self.index = 0
+        self.image = None
+        self.rect = None
+
+    def addImage(self, image:Image):
+        self.images.append(image)
+        if self.rect == None:
+            self.rect = image.rect
+
+    def update(self):
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+        self.image = self.images[self.index].data
+        
     
