eaeccb77e3f3c82ca24a2c62f32390e086e18c4a
Byron Cook <Byron.a.cook@gmail.com>
2017-12-11 10:54:19 +0100

    resize, randomize, cowboy

--- src/minigame2/Indian.py
+++ src/minigame2/Indian.py
@@ -11,20 +11,20 @@ class Indian():
         random_number = randint(1,9)
 
         if  random_number == 1:
-            return [50,50]
+            return [100, 100]
         if  random_number == 2:
-            return [50,100]
+            return [100, 200]
         if  random_number == 3:
-            return [50,150]
+            return [100, 300]
         if  random_number == 4:
-            return [100,50]
+            return [200, 100]
         if  random_number == 5:
-            return [100,100]
+            return [200, 200]
         if  random_number == 6:
-            return [100,150]
+            return [200, 300]
         if  random_number == 7:
-            return [150,50]
+            return [300,100]
         if  random_number == 8:
-            return [150,100]
+            return [300,200]
         if  random_number == 9:
-            return [150,150]
+            return [300,300]
--- src/minigame2/Whack.py
+++ src/minigame2/Whack.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import pygame as py
 
 from src.helper.screen_base import ScreenBase
@@ -12,12 +14,14 @@ class Whack(ScreenBase):
         self.game.drawer.clear()
         self.game.drawer.add_background_image("resources/graphics/minigame_2/background.jpg")
         self.points = 0
-        self.lives = 3
+        self.lives = 1000
         self.DisplayNewIndian = True;
         self.passed_time = 0
         self.location = Indian.get_location(self)
-        self.rect = py.Rect(self.location, [50,50])
+        self.rect = py.Rect(self.location, [100,100])
         self.mouse_position = None
+        self.color = [255, 0, 0]
+
         py.draw.rect(self.game.py_screen, [255, 0, 0], self.rect)
 
 
@@ -26,19 +30,27 @@ class Whack(ScreenBase):
             if event.type == py.MOUSEBUTTONDOWN and event.button == 1:
                 self.mouse_position = py.mouse.get_pos()
                 self.game.logger.info(str(self.mouse_position) + " lives: " + str(self.lives) + " points: " + str(self.points))
-                if self.rect.collidepoint(self.mouse_position):
-                    self.points += 1
-                else:
-                    if self.lives > 0:
-                        self.lives -= 1
-                        if self.lives == 0:
-                            self.points == 9999 #TODO engame
-                            self.game.logger.info("points: " + str(self.points))
+                if self.rect != None:
+                    if self.rect.collidepoint(self.mouse_position):
+                        if self.color == [255, 0, 0]:
+                            self.points += 1
+                            self.rect = None
+                        else:
+                            self.lives -= 1
+                            self.rect = None
+                    else:
+                        if self.lives > 0:
+                            self.lives -= 1
+                            if self.lives == 0:
+                                self.points = 9999 #TODO engame
+                                self.game.logger.info("points: " + str(self.points))
 
         return
 
     def on_update(self):
         self.passed_time += self.game.clock.get_time()
+       # if self.lives == 0:
+
         return
 
     def on_render(self):
@@ -46,9 +58,10 @@ class Whack(ScreenBase):
         if self.passed_time > self.get_speed_in_miliseconds():
             self.passed_time = 0
             self.location = Indian.get_location(self)
-            self.rect.x = self.location[0] #py.Rect(self.location, [50, 50])
-            self.rect.y = self.location[1]
-        py.draw.rect(self.game.py_screen, [255, 0, 0], self.rect)
+            self.rect = py.Rect(self.location, [100, 100])
+            self.change_color()
+        if self.rect != None:
+            py.draw.rect(self.game.py_screen, self.color, self.rect)
 
         py.display.update()
 
@@ -63,14 +76,20 @@ class Whack(ScreenBase):
 
     def display_Indian(self):
         if self.DisplayNewIndian == True:
-
             self.DisplayNewIndian = False
         elif self.DisplayNewIndian == False:
-
             self.DisplayNewIndian == True
         return
 
     def get_speed_in_miliseconds(self):
         if self.points > 2:
             return 500
-        return 5000
+        return 3000
+
+    def change_color(self):
+        random_number = randint(1, 2)
+        if random_number == 1:
+            self.color = [0, 255, 0]
+        else:
+            self.color = [255, 0 ,0]
+        return
