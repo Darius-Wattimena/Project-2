c8c59cd6c7224707e79e31890c477cac3455b533
Martin Drok <martin.symb@gmail.com>
2017-12-14 15:34:22 +0100

    Minigame5: A lot of changes, concept game is done. fixed target locations, added shooting mechanic targets now disappear when shot (image no longer drawn + rect is moved out of screen until next target spawns) added drawn gun images which change dependant on crosshair x pos, added gun shot and impact images of which animation plays on gun fire and disappear with help of event timer, added collision, added when collision is true crosshair img color swap added more accurate game timer, swapped from tracking FPS to using python time module added score: succesful shot = 10, missed shot = 5 added visual score text added reload mechanic which reloads gun with help of event timer to prevent spamming gun fire, added visual text when shot is succesful or misses, disappears with help of event timer, added keybindings to change spawn speed and cursor speed

--- src/minigame5/Target.py
+++ src/minigame5/Target.py
@@ -6,7 +6,7 @@ class Target():
         return
 
     def random_location(self):
-        random_number = randint(1,22)
+        random_number = randint(1, 22)
 
         if  random_number == 1:
             return [30,250]
@@ -19,36 +19,36 @@ class Target():
         if  random_number == 5:
             return [430,270]
         if  random_number == 6:
-            return [600,335]
+            return [620,280]
         if  random_number == 7:
-            return [790,315]
+            return [790,250]
         if  random_number == 8:
-            return [890,332]
+            return [910,280]
         if  random_number == 9:
-            return [940,340]
+            return [965,290]
         if  random_number == 10:
-            return [1030,334]
+            return [1025,280]
         if  random_number == 11:
-            return [1120,328]
+            return [1100,270]
         if  random_number == 12:
-            return [1180,326]
+            return [1160,270]
         if  random_number == 13:
-            return [1260,312]
+            return [1230,242]
         if  random_number == 14:
-            return [35,500]
+            return [5,450]
         if  random_number == 15:
-            return [157,493]
+            return [137,433]
         if  random_number == 16:
-            return [280,475]
+            return [250,410]
         if  random_number == 17:
-            return [370,470]
+            return [340,410]
         if  random_number == 18:
-            return [450,450]
+            return [420,385]
         if  random_number == 19:
-            return [825,450]
+            return [795,385]
         if  random_number == 20:
-            return [935,440]
+            return [910,380]
         if  random_number == 21:
-            return [1010,475]
+            return [990,415]
         if  random_number == 22:
-            return [1185,530]
+            return [1165,490]
