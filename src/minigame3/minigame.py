11604d000d822090ed76c84347b4b3429526923e
Dennis Luijkenaar <dennisluijkenaar@gmail.com>
2017-12-16 16:50:38 +0100

    Includes minigame 3 basic functions

--- src/minigame3/minigame.py
+++ src/minigame3/minigame.py
@@ -3,11 +3,13 @@ from ..helper.screen_base import ScreenBase
 from .enemy import enemy
 from src.helper.drawer import Drawer
 
+
 class Minigame_3(ScreenBase):
     def __init__(self, game):
         self.game = game
         self.game.set_screen(self)
         self.enemy_is_spawned = True
+        self.enemy_is_moving = True
         self.groundcolor = (240, 230, 140)
         self.skycolor = (20, 20, 255)
         self.player()
@@ -18,15 +20,17 @@ class Minigame_3(ScreenBase):
         self.skyrect = py.Rect([0, 0], [1280, 250])
 
         self.spawn_event = py.USEREVENT + 1
-        self.enemy_move_down = self.enemy_rect.move_ip(0, 10)
-        self.time_between_movement = 100
         self.time_between_spawn = 3000
+        py.time.set_timer(self.spawn_event, self.time_between_spawn)
 
+        self.move_event = py.USEREVENT + 2
+        self.time_between_movement = 5
+        py.time.set_timer(self.move_event, self.time_between_spawn)
 
     def player(self):
         self.loadchar = py.image.load("resources/graphics/minigame_3/testimage.png")
         self.char_rect = self.loadchar.get_rect()
-        self.player_rect = py.Rect([640, 650], [100,100])
+        self.player_rect = py.Rect([640, 650], [100, 100])
 
     def on_events(self, events):
         for event in events:
@@ -35,8 +39,11 @@ class Minigame_3(ScreenBase):
                 self.selected_location = enemy.get_location(self)
                 self.enemy_rect.x = self.selected_location[0]
                 self.enemy_rect.y = self.selected_location[1]
-                py.time.set_timer(self.enemy_move_down, self.time_between_movement)
                 py.time.set_timer(self.spawn_event, self.time_between_spawn)
+            if event.type == self.move_event:
+                self.enemy_is_moving = True
+                self.enemy_rect.move_ip(0, 2)
+                py.time.set_timer(self.move_event, self.time_between_movement)
 
     def on_update(self):
         return
@@ -50,6 +57,9 @@ class Minigame_3(ScreenBase):
         if self.enemy_is_spawned == True:
             py.draw.rect(self.game.py_screen, [0, 0, 0], self.enemy_rect)
             self.game.py_screen.blit(self.enemy_image, self.enemy_rect)
+        if  self.enemy_is_moving == True:
+            py.draw.rect(self.game.py_screen, [0, 0, 0], self.enemy_rect)
+            self.game.py_screen.blit(self.enemy_image, self.enemy_rect)
         py.display.update()
 
     def handle_mouse_input(self, mouse):
@@ -61,9 +71,9 @@ class Minigame_3(ScreenBase):
         elif keys[py.K_d]:
             self.player_rect.move_ip(10, 0)
         elif keys[py.K_w]:
-            self.player_rect.move_ip(0, -10)
+            self.enemy_rect.move_ip(0, 5)
         elif keys[py.K_s]:
-            self.player_rect.move_ip(0, 10)
+            self.enemy_rect.move_ip(0, -1)
         return
 
     def handle_mouse_position(self, mouse_position):
