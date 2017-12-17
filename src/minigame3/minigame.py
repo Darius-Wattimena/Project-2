833a41a4cd6b622e0b6b2165839708873b6a8b6e
Darius Wattimena <dariuswattimena@hotmail.com>
2017-12-17 19:37:46 +0100

    Added collision for minigame 3. Also added lives label.

--- src/minigame3/minigame.py
+++ src/minigame3/minigame.py
@@ -1,4 +1,6 @@
 import pygame as py
+
+from src.helper.label import Label
 from ..helper.screen_base import ScreenBase
 from .enemy import enemy
 from src.helper.drawer import Drawer
@@ -12,12 +14,12 @@ class Minigame_3(ScreenBase):
         self.enemy_is_moving = True
         self.groundcolor = (240, 230, 140)
         self.skycolor = (20, 20, 255)
-        self.player()
-        self.enemy_image = py.image.load("resources/graphics/minigame_3/testimage.png")
-        self.enemy_location = enemy.get_location(self)
-        self.enemy_rect = py.Rect(self.enemy_location, [100, 100])
+
         self.groundrect = py.Rect([0, 250], [1280, 720])
         self.skyrect = py.Rect([0, 0], [1280, 250])
+
+        self.player()
+        self.enemy()
 
         self.spawn_event = py.USEREVENT + 1
         self.time_between_spawn = 3000
@@ -29,11 +31,25 @@ class Minigame_3(ScreenBase):
 
         self.collide_event = py.USEREVENT + 3
 
+        self.lives_label = Label(self.game.py_screen, "Current Lives: " + str(self.lives), [0, 0, 0], 50)
+
     def player(self):
-        self.loadchar = py.image.load("resources/graphics/minigame_3/testimage.png")
-        self.char_rect = self.loadchar.get_rect()
-        self.player_rect = py.Rect([640, 650], [100, 100])
-        self.lives = 0
+        player_width = 100
+        player_height = 100
+        self.player_image = py.image.load("resources/graphics/minigame_3/testimage.png")
+        # Resize je foto omdat de photo kleiner is dan de player rect
+        self.player_image = py.transform.scale(self.player_image, [player_width, player_height])
+        self.player_rect = py.Rect([640, 650], [player_width, player_height])
+        self.lives = 100
+
+    def enemy(self):
+        enemy_width = 100
+        enemy_height = 100
+        self.enemy_image = py.image.load("resources/graphics/minigame_3/testimage.png")
+        # Resize je foto omdat de photo kleiner is dan de enemy rect
+        self.enemy_image = py.transform.scale(self.enemy_image, [enemy_width, enemy_height])
+        self.enemy_location = enemy.get_location(self)
+        self.enemy_rect = py.Rect(self.enemy_location, [enemy_width, enemy_height])
 
     def on_events(self, events):
         for event in events:
@@ -42,34 +58,37 @@ class Minigame_3(ScreenBase):
                 self.selected_location = enemy.get_location(self)
                 self.enemy_rect.x = self.selected_location[0]
                 self.enemy_rect.y = self.selected_location[1]
-                py.time.set_timer(self.spawn_event, self.time_between_spawn)
             if event.type == self.move_event:
                 self.enemy_is_moving = True
                 self.enemy_rect.move_ip(0, 2)
                 py.time.set_timer(self.move_event, self.time_between_movement)
             if event.type == self.collide_event:
-                if self.enemy_rect.colliderect(self.player_rect):
-                    if self.lives > 1:
+                collision = self.player_rect.colliderect(self.enemy_rect)
+                if collision:
+                    # TODO despawn enemy when collided
+                    if self.lives > 0:
                         self.lives -= 1
-                    elif self.lives == 1:
-                        py.quit()
-                        quit()
+                        self.lives_label.text = "Current Lives: " + str(self.lives)
+                    elif self.lives == 0:
+                        self.lives_label.text = "You Died!!"
 
     def on_update(self):
-        return
+        # Elke frame roepen wij deze event aan en kijken we of er collision is
+        py.event.post(py.event.Event(self.collide_event))
 
     def on_render(self):
         self.game.drawer.draw_canvas()
         py.draw.rect(self.game.py_screen, self.groundcolor, self.groundrect)
         py.draw.rect(self.game.py_screen, self.skycolor, self.skyrect)
-        self.game.py_screen.blit(self.loadchar, self.player_rect)
+        self.game.py_screen.blit(self.player_image, self.player_rect)
 
-        if self.enemy_is_spawned == True:
+        if self.enemy_is_spawned is True:
             py.draw.rect(self.game.py_screen, [0, 0, 0], self.enemy_rect)
             self.game.py_screen.blit(self.enemy_image, self.enemy_rect)
-        if  self.enemy_is_moving == True:
+        if self.enemy_is_moving is True:
             py.draw.rect(self.game.py_screen, [0, 0, 0], self.enemy_rect)
             self.game.py_screen.blit(self.enemy_image, self.enemy_rect)
+        self.lives_label.render(100, 100)
         py.display.update()
 
     def handle_mouse_input(self, mouse):
