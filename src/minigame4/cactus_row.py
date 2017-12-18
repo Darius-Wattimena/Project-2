b5ab17e073928e50755517e838d3ae6ff693d533
Maad <0947046@hr.nl>
2017-12-18 13:39:22 +0100

    Added finish line.
    Timer now counts down.
    Cactuses now despawn when the player hit them.
    The player losses time when it hits a cactus.
    Spawn finish line when a number of cactuses has been spawned.
    Added multiple cactus images.

--- src/minigame4/cactus_row.py
+++ src/minigame4/cactus_row.py
@@ -5,13 +5,13 @@ from src.minigame4.cactus import Cactus
 
 
 class CactusRow:
-    def __init__(self, screen, cactus_image):
+    def __init__(self, screen, cactus_images):
         self.items = []
         self.x = screen.get_width()
         self.cactus_width = 100
         self.cactus_height = 100
         self.row_speed = 1
-        self.cactus_image = cactus_image
+        self.cactus_images = cactus_images
         self.screen = screen
         self.rect = py.Rect([self.x, 0], [self.cactus_width, screen.get_height()])
 
@@ -20,6 +20,9 @@ class CactusRow:
         # TODO spawn meerdere cactusen
 
         random_integer = randint(1, 13)
+        random_cactus_image_index = randint(1, 3)
+        random_cactus_image_index -= 1
+        random_cactus_image = self.cactus_images[random_cactus_image_index]
 
         cactus_spawn_map = [
             0,
@@ -38,7 +41,7 @@ class CactusRow:
         ]
         random_index = random_integer - 1
         cactus_y = cactus_spawn_map[random_index]
-        cactus = Cactus(self.screen, self.x, cactus_y, self.cactus_width, self.cactus_height, self.cactus_image)
+        cactus = Cactus(self.screen, self.x, cactus_y, self.cactus_width, self.cactus_height, random_cactus_image)
         self.items.append(cactus)
 
     def move_row_left(self):
--- src/minigame4/minigame.py
+++ src/minigame4/minigame.py
@@ -13,14 +13,13 @@ class Minigame_4(ScreenBase):
         self.background_image = py.image.load("resources/graphics/minigame_4/background3.jpg")
         self.player_image = py.image.load("resources/graphics/minigame_4/hourse.jpg")
         self.background_image_rect = self.background_image.get_rect()
-        self.player_rect = self.player_image.get_rect()
 
         self.player_x = 640
         self.player_y = 360
         self.player_speed = 10
-        self.player_rect = py.Rect([self.player_x, self.player_y], [40, 40])
+        self.player_rect = py.Rect([self.player_x, self.player_y], [100, 75])
 
-        self.time = 0
+        self.time = 120000
         self.timer_label = Label(self.game.py_screen, "", [254, 254, 254], 50)
 
         # Dit zijn eigen events die we aanroepen via de timer class en handelen in de on_events methode
@@ -28,10 +27,21 @@ class Minigame_4(ScreenBase):
         self.move_cactus_event = py.USEREVENT + 2
 
         # Laad de cactus image in die we later aan elke cactus class meegeven
-        self.cactus_image = py.image.load("resources/graphics/minigame_4/cactus.png")
-
+        self.cactus_images = []
+        self.cactus_images.append(py.image.load("resources/graphics/minigame_4/cactus.png"))
+        self.cactus_images.append(py.image.load("resources/graphics/minigame_4/cactus3.png"))
+        self.cactus_images.append(py.image.load("resources/graphics/minigame_4/cactus4.png"))
         # Maak een array aan waar we elke cactus_rij instoppen
         self.cactus_rows = []
+        self.total_cactus_spawned = 0
+        finish_x = self.game.py_screen.get_width()
+        finish_y = 0
+        finish_width = 100
+        finish_height = self.game.py_screen.get_height()
+        self.finish_line_rect = py.Rect([finish_x, finish_y], [finish_width, finish_height])
+        self.spawn_finish_line = False
+
+        self.finished_time = -1
 
         # Hiermee roepen we de spawn cactus event aan elke 2000ms en de move event elke 20 ms
         # https://www.pygame.org/docs/ref/time.html#pygame.time.set_timer
@@ -63,15 +73,22 @@ class Minigame_4(ScreenBase):
         for event in events:
             # Als er een nieuwe cactuse spawn event wordt aangeroepen maak dan een nieuwe cactus row aan en zet die in de bestaande rijën
             if event.type == self.spawn_cactus_event:
-                # TODO maak een cactus_finish aan.
-                # Hier stopt de minigame als de player dit haalt en wordt de timer gestopt.
-
-                new_cactus_row = CactusRow(self.game.py_screen, self.cactus_image)
-                new_cactus_row.create_row()
-                self.cactus_rows.append(new_cactus_row)
+                if self.total_cactus_spawned == -1:
+                    pass
+                elif self.total_cactus_spawned >= 3:
+                    self.total_cactus_spawned = -1
+                    self.spawn_finish_line = True
+                else:
+                    self.total_cactus_spawned += 1
+                    new_cactus_row = CactusRow(self.game.py_screen, self.cactus_images)
+                    new_cactus_row.create_row()
+                    self.cactus_rows.append(new_cactus_row)
             elif event.type == self.move_cactus_event:
                 for cactus_row in self.cactus_rows:
                     cactus_row.move_row_left()
+                if self.spawn_finish_line:
+                    self.finish_line_rect.move_ip(-1, 0)
+
 
     def on_update(self):
         # TODO check of de player een cactus raakt. Zo ja geef een penalty en verwijder de cactus van het scherm.
@@ -80,10 +97,24 @@ class Minigame_4(ScreenBase):
             # Als de cactus row uit het scherm zit remove hem dan uit de current rows
             if cactus_row.rect.right < 0:
                 self.cactus_rows.remove(cactus_row)
+            for cactus in cactus_row.items:
+                if self.player_rect.colliderect(cactus.rect):
+                    cactus_row.items.remove(cactus)
+                    self.time -= 5000
+                    # TODO add strafpunten
+        if self.spawn_finish_line:
+            if self.player_rect.colliderect(self.finish_line_rect):
+                if self.finished_time == -1:
+                    self.finished_time = self.time
 
+
+      # Kijk of de player rect collied met de finish line rect
         # TODO rework timer
-        self.time += 1
-        self.timer_label.text = str(self.time)
+        if self.finished_time != -1:
+            self.timer_label.text = "Finished with: " + str(self.finished_time)
+        else:
+            self.time -= 1
+            self.timer_label.text = str(self.time)
         pass
 
     def on_render(self):
@@ -93,6 +124,9 @@ class Minigame_4(ScreenBase):
         # Render elke cactus_rows die we hebben
         for cactus_row in self.cactus_rows:
             cactus_row.render()
-        self.game.py_screen.blit(self.player_image, self.player_rect)
+        if self.spawn_finish_line:
+            py.draw.rect(self.game.py_screen, [255, 255, 255], self.finish_line_rect)
+        # self.game.py_screen.blit(self.player_image, self.player_rect)
+        py.draw.rect(self.game.py_screen, [255, 255, 255], self.player_rect)
         self.timer_label.render(500, 100)
         py.display.update()
