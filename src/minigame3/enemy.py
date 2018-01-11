6567e6eb8be9b0ee384295c29b68a092f4224b86
Darius Wattimena <dariuswattimena@hotmail.com>
2018-01-11 13:23:55 +0100

    Fixed bugs in minigame 3 and refactored most of the code

--- src/minigame3/enemy.py
+++ src/minigame3/enemy.py
@@ -1,27 +1,26 @@
-import pygame as py
-
 from random import randint
 
-class enemy():
+
+class Enemy:
     def __init__(self):
         return
 
     def get_location(self):
-        random_number = randint(1,8)
+        random_number = randint(1, 8)
 
-        if  random_number == 1:
-            return [0,250]
-        if  random_number == 2:
-            return [160,250]
-        if  random_number == 3:
-            return [320,250]
-        if  random_number == 4:
-            return [480,250]
-        if  random_number == 5:
-            return [640,250]
-        if  random_number == 6:
-            return [800,250]
-        if  random_number == 7:
-            return [960,250]
-        if  random_number == 8:
-            return [1120,250]
+        if random_number == 1:
+            return [0, 250]
+        elif random_number == 2:
+            return [160, 250]
+        elif random_number == 3:
+            return [320, 250]
+        elif random_number == 4:
+            return [480, 250]
+        elif random_number == 5:
+            return [640, 250]
+        elif random_number == 6:
+            return [800, 250]
+        elif random_number == 7:
+            return [960, 250]
+        elif random_number == 8:
+            return [1120, 250]
--- src/minigame3/minigame.py
+++ src/minigame3/minigame.py
@@ -1,12 +1,11 @@
 import pygame as py
 
 from src.helper.label import Label
-from ..helper.screen_base import ScreenBase
-from .enemy import enemy
+from src.helper.screen_base import ScreenBase
 from src.minigame1.fight import PauseScreenButton
 from src.minigame1.ui.header_label import HeaderLabel
 from src.minigame1.ui.text_label import TextLabel
-from src.helper.drawer import Drawer
+from src.minigame3.enemy import Enemy
 
 
 class Minigame_3(ScreenBase):
@@ -14,19 +13,23 @@ class Minigame_3(ScreenBase):
         self.game = game
         self.game.set_screen(self)
         self.enemy_is_spawned = True
-        self.wingame = False
         self.enemy_is_moving = True
         self.player_is_collided = False
         self.coyote_is_spawned = True
         self.coyote_is_moving = True
-        self.startgame = False
-        self.endgame = False
-        self.wingame = False
+        self.start_game = False
+        self.end_game = False
+        self.win_game = False
         self.groundcolor = (240, 230, 140)
         self.skycolor = (20, 20, 255)
 
         self.groundrect = py.Rect([0, 250], [1280, 720])
         self.skyrect = py.Rect([0, 0], [1280, 250])
+
+        self.beginning_lives = 2
+        self.lives = self.beginning_lives
+        self.player_start_pos = [640, 650]
+        self.coyote_start_pos = [640, 250]
 
         self.player()
         self.enemy()
@@ -76,7 +79,6 @@ class Minigame_3(ScreenBase):
         self.win_label_text_2 = TextLabel(self.game.py_screen, self.win_text[2])
         self.win_label_text_3 = TextLabel(self.game.py_screen, self.win_text[3])
 
-
         screen_center_width = self.game.py_screen.get_width() / 2
         self.button_x = screen_center_width - (self.btn[0].width / 2)
 
@@ -98,14 +100,35 @@ class Minigame_3(ScreenBase):
 
         self.lives_label = Label(self.game.py_screen, "Current Lives: " + str(self.lives), [0, 0, 0], 50)
 
+    def start(self):
+        self.end_game = False
+        self.win_game = False
+        self.start_game = True
+
+    def reset(self):
+        # Reset lives and the label
+        self.lives = self.beginning_lives
+        self.lives_label.text = "Current Lives: " + str(self.lives)
+
+        # Reset position of player and the coyote
+        self.player_rect.x = self.player_start_pos[0]
+        self.player_rect.y = self.player_start_pos[1]
+        self.coyote_location = self.coyote_start_pos
+        self.coyote_rect.x = self.coyote_start_pos[0]
+        self.coyote_rect.y = self.coyote_start_pos[1]
+
+        # Start the minigame
+        self.start()
+
+
     def player(self):
         player_width = 100
         player_height = 100
         self.player_image = py.image.load("resources/graphics/minigame_3/testimage.png")
         # Resize je foto omdat de photo kleiner is dan de player rect
         self.player_image = py.transform.scale(self.player_image, [player_width, player_height])
-        self.player_rect = py.Rect([640, 650], [player_width, player_height])
-        self.lives = 2
+        self.player_rect = py.Rect(self.player_start_pos, [player_width, player_height])
+        self.lives = self.beginning_lives
 
     def enemy(self):
         enemy_width = 100
@@ -113,7 +136,7 @@ class Minigame_3(ScreenBase):
         self.enemy_image = py.image.load("resources/graphics/minigame_3/testimage.png")
         # Resize je foto omdat de photo kleiner is dan de enemy rect
         self.enemy_image = py.transform.scale(self.enemy_image, [enemy_width, enemy_height])
-        self.enemy_location = enemy.get_location(self)
+        self.enemy_location = Enemy().get_location()
         self.enemy_rect = py.Rect(self.enemy_location, [enemy_width, enemy_height])
 
     def coyote(self):
@@ -121,16 +144,15 @@ class Minigame_3(ScreenBase):
         coyote_height = 100
         self.coyote_image = py.image.load("resources/graphics/minigame_3/testimage.png")
         self.coyote_image = py.transform.scale(self.coyote_image, [coyote_width, coyote_height])
-        self.enemy_placement = (640, 250)
-        self.coyote_location = self.enemy_placement
+        self.coyote_location = self.coyote_start_pos
         self.coyote_rect = py.Rect(self.coyote_location, [coyote_width, coyote_height])
 
     def on_events(self, events):
         for event in events:
-            if self.startgame == True and self.endgame == False:
+            if self.start_game is True:
                 if event.type == self.spawn_event:
                     self.enemy_is_spawned = True
-                    self.selected_location = enemy.get_location(self)
+                    self.selected_location = Enemy().get_location()
                     self.enemy_rect.x = self.selected_location[0]
                     self.enemy_rect.y = self.selected_location[1]
                     self.player_is_collided = False
@@ -141,33 +163,29 @@ class Minigame_3(ScreenBase):
                 if event.type == self.collide_event:
                     collision = self.player_rect.colliderect(self.enemy_rect)
                     if collision:
-                        if self.player_is_collided == False:
-                            # TODO despawn enemy when collided
-                            if self.lives > 1:
+                        if self.player_is_collided is False:
+                            if self.lives >= 1:
                                 self.lives -= 1
                                 self.player_is_collided = True
                                 self.lives_label.text = "Current Lives: " + str(self.lives)
-                            elif self.lives == 0:
-                                self.endgame = True
-                                self.startgame = False
+
+                            # End the game when 0 lives are left
+                            if self.lives == 0:
+                                self.end_game = True
+                                self.start_game = False
                 if event.type == self.coyote_event:
                     self.coyote_is_spawned = True
                 self.coyote_selected_location = self.coyote_location
                 if event.type == self.coyote_move_event:
                     self.coyote_is_moving = True
                     self.coyote_rect.move_ip(0, 1)
-            if self.startgame == False:
+            else:
                 if event.type == py.MOUSEBUTTONDOWN:
                     if self.btn[0].is_clicked(self.mouse_position):
-                        if self.endgame == False:
-                            self.startgame = True
-                            self.endgame = False
-                            self.wingame = False
-                        elif self.endgame == True or self.wingame == True:
-                            self.__init__(self.game)
-                            self.endgame = False
-                            self.startgame = True
-                            self.wingame = False
+                        if self.end_game is False:
+                            self.start()
+                        else:
+                            self.reset()
                     elif self.btn[1].is_clicked(self.mouse_position):
                         from src.global_screens.pick_minigame import PickMinigame
                         self.game.drawer.clear()
@@ -178,7 +196,7 @@ class Minigame_3(ScreenBase):
         py.event.post(py.event.Event(self.collide_event))
 
     def on_render(self):
-        if self.startgame:
+        if self.start_game:
             self.game.drawer.draw_canvas()
             py.draw.rect(self.game.py_screen, self.groundcolor, self.groundrect)
             py.draw.rect(self.game.py_screen, self.skycolor, self.skyrect)
@@ -194,47 +212,46 @@ class Minigame_3(ScreenBase):
                 py.draw.rect(self.game.py_screen, [0, 0, 0], self.coyote_rect)
                 self.game.py_screen.blit(self.coyote_image, self.coyote_rect)
             self.lives_label.render(100, 100)
-        elif self.startgame == False and self.endgame == False and self.wingame == False:
-            self.game.drawer.draw_canvas()
-            # Draw an dark surface on the screen
-            self.game.py_screen.blit(self.dark_surface, [0, 0])
+        else:
+            if self.end_game is False:
+                self.game.drawer.draw_canvas()
+                # Draw an dark surface on the screen
+                self.game.py_screen.blit(self.dark_surface, [0, 0])
 
-            # Draw objective
-            self.objective_label_header.render(465, 190)
-            self.objective_label_text_1.render(465, 240)
-            self.objective_label_text_2.render(465, 260)
-            self.objective_label_text_3.render(465, 280)
+                # Draw objective
+                self.objective_label_header.render(465, 190)
+                self.objective_label_text_1.render(465, 240)
+                self.objective_label_text_2.render(465, 260)
+                self.objective_label_text_3.render(465, 280)
 
-            # Draw controlls
-            self.controls_label_header.render(465, 320)
-            self.controls_label_text_1.render(465, 370)
-            self.controls_label_text_2.render(465, 420)
-            self.controls_label_text_3.render(465, 470)
+                # Draw controlls
+                self.controls_label_header.render(465, 320)
+                self.controls_label_text_1.render(465, 370)
+                self.controls_label_text_2.render(465, 420)
+                self.controls_label_text_3.render(465, 470)
 
-            self.btn[1].render(self.mouse_position, self.button_x, 640)
-            self.btn[0].render(self.mouse_position, self.button_x, 560)
+                self.btn[1].render(self.mouse_position, self.button_x, 640)
+                self.btn[0].render(self.mouse_position, self.button_x, 560)
+            elif self.win_game is False:
+                self.game.py_screen.blit(self.dark_surface, [0, 0])
 
-        elif self.startgame == False and self.endgame == True and self.wingame == False:
-            self.game.py_screen.blit(self.dark_surface, [0, 0])
+                self.death_label_header.render(465, 190)
+                self.death_label_text_1.render(465, 240)
+                self.death_label_text_2.render(465, 260)
 
-            self.death_label_header.render(465, 190)
-            self.death_label_text_1.render (465, 240)
-            self.death_label_text_2.render (465, 260)
+                self.btn[0].render(self.mouse_position, self.button_x, 560)
+                self.btn[1].render(self.mouse_position, self.button_x, 640)
 
-            self.btn[0].render(self.mouse_position, self.button_x, 560)
-            self.btn[1].render(self.mouse_position, self.button_x, 640)
+            elif self.win_game is True:
+                self.game.py_screen.blit(self.dark_surface, [0, 0])
 
-        elif self.wingame == True and self.endgame == False and self.startgame == False:
-            self.game.py_screen.blit(self.dark_surface, [0, 0])
+                self.win_label_header.render(465, 190)
+                self.win_label_text_1.render(465, 240)
+                self.win_label_text_2.render(465, 260)
+                self.win_label_text_3.render(465, 280)
 
-            self.win_label_header.render(465, 190)
-            self.win_label_text_1.render(465, 240)
-            self.win_label_text_2.render(465, 260)
-            self.win_label_text_3.render(465, 280)
-
-            self.btn[0].render(self.mouse_position, self.button_x, 560)
-            self.btn[1].render(self.mouse_position, self.button_x, 640)
-
+                self.btn[0].render(self.mouse_position, self.button_x, 560)
+                self.btn[1].render(self.mouse_position, self.button_x, 640)
 
         py.display.update()
 
@@ -243,7 +260,6 @@ class Minigame_3(ScreenBase):
 
     def handle_mouse_position(self, mouse_position):
         self.mouse_position = mouse_position
-        return
 
     def handle_key_input(self, keys):
         if keys[py.K_a]:
@@ -260,7 +276,7 @@ class Minigame_3(ScreenBase):
             coyote_collision = self.player_rect.colliderect(self.coyote_rect)
             if coyote_collision:
                 self.lives_label.text = "You won!"
-                self.wingame = True
-                self.startgame = False
-                self.endgame = False
+                self.win_game = True
+                self.start_game = False
+                self.end_game = True
         return
